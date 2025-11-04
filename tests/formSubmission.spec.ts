be6278487bea3a340e107d634cf14882bf4eb4ee
daryltee91 <daryltee@gmail.com>
2025-11-04 12:37:36 +0800

    Add initial implementation for test case 1

---
+++ tests/formSubmission.spec.ts
@@ -0,0 +1,73 @@
+import { test, expect } from "@playwright/test";
+
+const datasets = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    email: "john.doe@example.com",
+    gender: "Male",
+    mobile: "1234567890",
+    subjects: ["Maths"],
+    hobbies: ["Sports", "Music"],
+    address: {
+      block: "123",
+      street: "Test Street, Test City",
+      unit: "",
+      level: "",
+      building: "",
+      postal: "",
+    },
+    state: "NCR",
+    city: "Delhi",
+  },
+];
+
+test("submit first dataset", async ({ page }) => {
+  await page.goto("https://demoqa.com/automation-practice-form");
+
+  // Wait for form to be visible
+  await expect(page.locator("#userForm")).toBeVisible();
+
+  // Scroll userForm to top of page
+  // This is done to avoid any bottom floating advertisement banners covering form elements
+  await page.evaluate(() => {
+    document.getElementById("userForm")?.scrollIntoView(true);
+  });
+
+  // Fill First Name
+  await page.getByPlaceholder("First Name").fill(datasets[0].firstName);
+
+  // Fill Last Name
+  await page.getByPlaceholder("Last Name").fill(datasets[0].lastName);
+
+  // Fill Email
+  await page.getByPlaceholder("name@example.com").fill(datasets[0].email);
+
+  // Fill Mobile Number
+  await page.getByPlaceholder("Mobile Number").fill(datasets[0].mobile);
+
+  // Select Gender
+  await page.getByLabel(datasets[0].gender, { exact: true }).click({ force: true }); // Force the click on the input, as the label intercepts it and will prevent the click.
+
+  // Set Date of Birth
+  await page.locator("#dateOfBirthInput").click();
+  await page.locator(".react-datepicker__year-select").selectOption("1990");
+  await page.locator(".react-datepicker__month-select").selectOption("4");
+  await page.locator(".react-datepicker__day--015").click();
+
+  // Fill subjects
+  for (const subject of datasets[0].subjects) {
+    await page.locator(".subjects-auto-complete__value-container").pressSequentially(subject);
+    await page.locator(".subjects-auto-complete__value-container").press("Enter");
+  }
+
+  // Set picture
+  await page.getByLabel("Select picture").setInputFiles("assets/images/example.png");
+
+  // Fill Current Address
+  await page
+    .locator("#currentAddress")
+    .fill(
+      `${datasets[0].address.block} ${datasets[0].address.street}; #${datasets[0].address.level}-${datasets[0].address.unit}; ${datasets[0].address.building}; Singapore Postal code ${datasets[0].address.postal}`
+    );
+});
